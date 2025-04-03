e1e6f6253e56af3069bbe76dfa7ab6b12aa86502
Fields <c27samuel.fields@afacademy.af.edu>
2025-04-03 14:51:05 -0600

    SUbmission 1

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,105 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+    port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+    signal w_clk : std_logic := '0';
+    signal w_reset : std_logic := '0';
+    signal w_left : std_logic := '0';
+    signal w_right : std_logic := '0';
+    
+    signal w_lightsL : std_logic_vector(2 downto 0) := "000";
+    signal w_lightsR : std_logic_vector(2 downto 0) := "000";
+	
+	
+	signal w_S : STD_LOGIC_vector(2 downto 0) :="000";
+	signal w_S_next: std_logic_vector(2 downto 0)  :="000";
 	
 	-- constants
-	
+    constant k_clk_period : time := 10 ns;
+    
+ 
 	
 begin
 	-- PORT MAPS ----------------------------------------
+	   uut: thunderbird_fsm port map (
+          i_reset => w_reset,
+          i_clk => w_clk,
+          i_left => w_left,
+          i_right => w_right,
+          o_Lights_L => w_lightsL,
+          o_Lights_R => w_lightsR
+          
+
+        );
 	
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
+    	clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
+    
     
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+		sim_proc: process
+	begin
+		-- sequential timing		
+		w_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_lightsL = "000" report "bad reset" severity failure;
+		  
+		w_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_lightsR = "000" report "bad reset" severity failure;
+		
+		w_reset <= '0';
+		wait for k_clk_period*1;
+		
+		-- red light
+		w_C <= '0'; wait for k_clk_period;
+          assert w_stoplight = "100" report "should be red when no car" severity failure;
+		-- car shows up at red light
+        w_C <= '1'; wait for k_clk_period;
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+        wait for k_clk_period * 3; -- stay green
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+        -- go to yellow
+        w_C <= '0'; wait for k_clk_period;
+            assert w_stoplight = "010" report "should be yellow when cars done" severity failure;
+        wait for k_clk_period; -- time to go to red
+            assert w_stoplight = "100" report "did not go red after yellow" severity failure;
+        
+        -- reset and test yellow to red even if car
+        w_reset <= '1'; w_C <= '1';
+            wait for k_clk_period;
+        w_reset <= '0';
+          assert w_stoplight = "010" report "bad reset" severity failure;
+        wait for k_clk_period;
+            assert w_stoplight = "100" report "skipped red after yellow" severity failure;
+        wait for k_clk_period;
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+	
+		wait;
+	end process;
+	
+	
 	
 	-----------------------------------------------------	
-	
+	-- Markdown syntax for including an image in README.md  :
+--      ![Waveform from thunderbird_fsm_tb](myimage.png)
 end test_bench;
